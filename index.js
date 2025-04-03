e5e89154144fdaedc6dd0c9138be33a6e5ecbed6
unknown <blackskiesdancer@gmail.com>
2025-04-03 14:44:27 +1100

    reset button coded & colours

--- index.js
+++ index.js
@@ -27,13 +27,23 @@ pauseBtn.addEventListener("click", () => {
     }
 })
 resetBtn.addEventListener("click", () => {
+    paused = true;
+    clearInterval(intervalid);
+    startTime = 0;
+    elapsedTime = 0;
+    currentTime = 0;
+    hours = 0;
+    mins = 0;
+    secs = 0;
+    timeDisplay.textContent = "00:00:00"
+    document.body.style.backgroundColor = "";
 })
 
 function updateTime(){
     elapsedTime = Date.now() - startTime;
     secs = Math.floor ((elapsedTime / 1000) % 60)
     mins = Math.floor ((elapsedTime / (1000 * 60)) % 60)
-    hrs = Math.floor ((elapsedTime / (1000 * 60 * 60)) % 60)
+    hrs = Math.floor ((elapsedTime / (1000 * 60 * 60)) % 24)
 
     secs = pad(secs) 
     mins = pad(mins) 
@@ -44,4 +54,10 @@ function updateTime(){
     function pad(unit){
     return (("0") + unit).length > 2 ? unit : "0" + unit
 }
+
+    if (elapsedTime / 1000 >= 15) {
+        document.body.style.backgroundColor = "#fec8ff";
+    } else if (elapsedTime / 1000 >= 5) {
+        document.body.style.backgroundColor = "#8ac2b9";
+    }
 }
